520efcc7a473d0365d7e06e6570717f46860ad88
Teimir <49413936+Teimir@users.noreply.github.com>
2023-07-31 21:15:57 +0300

    Update settings.py

--- boloto/settings.py
+++ boloto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-cu8#9#j48ckewbjg4=j*3wrz05zsztip8j+55u4=r*zdjxclvb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['185.70.186.9']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 
