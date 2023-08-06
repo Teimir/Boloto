7eb8b48c6634532c37732623a2aac85636c736d8
Teimir <49413936+Teimir@users.noreply.github.com>
2023-08-06 22:05:16 +0300

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import New, Newa
+from .models import New, Newa, Post
 
 admin.site.register(New)
 admin.site.register(Newa)
+admin.site.register(Post)
