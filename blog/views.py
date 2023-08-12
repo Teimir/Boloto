e0687a33e02cde4100a2f4058e8e21c5dcaaa1b6
Teimir <49413936+Teimir@users.noreply.github.com>
2023-08-12 18:19:58 +0300

    Минорное обновление

--- blog/views.py
+++ blog/views.py
@@ -10,11 +10,7 @@ from django.http import QueryDict
 from django.shortcuts import render, get_object_or_404
 
 def home(request):
-    data = {
-        'news': New.objects.all(),
-        'title': 'Главная страница'
-    }
-    return render(request, 'blog/home.html', data)
+    return redirect("boloto")
 
 @login_required(login_url='login')
 def create(request):
